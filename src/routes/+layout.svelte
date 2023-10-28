cd92fbb5a613099a321ac2a8ffe7ec7b5178ca13
mahmoud-bored <mahmoud.public.404@gmail.com>
2023-10-28 20:57:24 +0300

    fix: build fail due to dynamic routes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script>
-	import { fade } from 'svelte/transition';
+	import { fly } from 'svelte/transition';
 
 	export let data;
 </script>
 
 {#key data.pathname}
-	<div in:fade={{ duration: 300, delay: 400 }} out:fade={{ duration: 300 }}>
+	<div in:fly={{ y: -600, duration: 300, delay: 400 }} out:fly={{ y: 600, duration: 300 }} style="overflow: hidden; background-color: #333;">
 		<slot />
 	</div>
 {/key}
