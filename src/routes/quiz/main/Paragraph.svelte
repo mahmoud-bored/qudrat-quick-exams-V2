0fc27232c596ba23f36f921ee3af0aff9d2b568f
Mahmoud Elwan <mahmoud.public.404@gmail.com>
2024-06-29 23:57:28 +0300

    Fix: if a short question comes after a long question the paragraph for the long question doesn't disappear

--- src/routes/quiz/main/Paragraph.svelte
+++ src/routes/quiz/main/Paragraph.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { fly } from "svelte/transition";
 	import { questionOutTransitionDuration } from "./quiz-main-stores";
-    export let paragraphText: string | null = "This is a paragraph"
+    export let paragraphText: string | null = null
 
     let inputValue: number = 25
     let paragraphFontSize: number
@@ -24,21 +24,22 @@
 <div class="container">
     <input type="range" bind:value={inputValue}>
     <div class="paragraph-field-container">
-        {#key formattedParagraphText}
-            <span 
-                in:fly={{ y:-20, x: 40, duration: 300 }} 
-                out:fly={{ x: -40, duration: $questionOutTransitionDuration }} 
-                style="font-size: {paragraphFontSize}em"
-            >
-                <p class="whitespace-pre-line">{formattedParagraphText ?? ""}</p>
-            </span>
-        {/key}
         {#if isParagraphEmpty}
-            <div class="lock-image-container" in:fly={{ y:-20, x: 40, duration: 300 }} out:fly={{ y: 20, x: -20, duration: $questionOutTransitionDuration }}>
-                <svg width="280" height="375" viewBox="0 0 280 375" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect y="154" width="280" height="221" rx="23" fill="black"/><circle cx="140" cy="249" r="28" fill="#454545"/><path d="M125 269H155L163.5 309H117L125 269Z" fill="#454545"/><path d="M206.5 93.502V155.502H242.5V91.502C238.833 61.1686 213.3 0.40195 140.5 0.00195016C67.7 -0.39805 41.1667 60.8353 37 91.502V155.502H73.5V91.502C76.8333 73.002 94.9 36.0019 140.5 36.0019C186.1 36.0019 203.5 74.3353 206.5 93.502Z" fill="black"/>
-                </svg>
-            </div>
+        <div class="lock-image-container" in:fly={{ y:-20, x: 40, duration: 300 }} out:fly={{ y: 20, x: -20, duration: $questionOutTransitionDuration }}>
+            <svg width="280" height="375" viewBox="0 0 280 375" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect y="154" width="280" height="221" rx="23" fill="black"/><circle cx="140" cy="249" r="28" fill="#454545"/><path d="M125 269H155L163.5 309H117L125 269Z" fill="#454545"/><path d="M206.5 93.502V155.502H242.5V91.502C238.833 61.1686 213.3 0.40195 140.5 0.00195016C67.7 -0.39805 41.1667 60.8353 37 91.502V155.502H73.5V91.502C76.8333 73.002 94.9 36.0019 140.5 36.0019C186.1 36.0019 203.5 74.3353 206.5 93.502Z" fill="black"/>
+            </svg>
+        </div>
+        {:else}
+            {#key paragraphText}
+                <span 
+                    in:fly={{ y:-20, x: 40, duration: 300 }} 
+                    out:fly={{ x: -40, duration: $questionOutTransitionDuration }} 
+                    style="font-size: {paragraphFontSize}em"
+                >
+                    <p class="whitespace-pre-line">{formattedParagraphText ?? ""}</p>
+                </span>
+            {/key}
         {/if}
     </div>
 </div>
